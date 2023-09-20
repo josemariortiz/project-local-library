bbfbc670f4d3edd87885f37efe3a2707ac92ee23
josemariortiz <136773792+josemariortiz@users.noreply.github.com>
2023-09-20 16:40:13 -0700

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,47 +1,52 @@
+function findAccountById(accounts, id) {
+  return accounts.find((account) => account.id === id);
+}
+
 function findAuthorById(authors, id) {
-  const foundId = authors.find((author) => author.id === id)
-  return foundId
+  const foundId = authors.find((author) => author.id === id);
+  return foundId;
 }
 
 function findBookById(books, id) {
-  const foundBook = books.find((book) => book.id === id)
-  return foundBook
+  const foundBook = books.find((book) => book.id === id);
+  return foundBook;
 }
 
 function partitionBooksByBorrowedStatus(books) {
-  const isNotAvailable = []
-  const isAvailable = []
+  const isNotAvailable = [];
+  const isAvailable = [];
 
   for (book of books) {
-    const borrowArray = book.borrows
-    const firstReturned = borrowArray[0]
+    const borrowArray = book.borrows;
+    const firstReturned = borrowArray[0];
 
-    if(firstReturned.returned) {
-      isAvailable.push(book)
+    if (firstReturned.returned) {
+      isAvailable.push(book);
     } else {
-      isNotAvailable.push(book)
+      isNotAvailable.push(book);
     }
   }
-  return [isNotAvailable, isAvailable]
+  return [isNotAvailable, isAvailable];
 }
 
 function getBorrowersForBook(book, accounts) {
-  const borrowers = []
+  const borrowers = [];
 
   for (const transaction of book.borrows) {
-    const accountId = transaction.id
-    const account =  accounts.find((acc) => acc.id === accountId)
-    account.returned = transaction.returned
-    borrowers.push(account)
+    const accountId = transaction.id;
+    const account = findAccountById(accounts, accountId); // Use the helper function
+    account.returned = transaction.returned;
+    borrowers.push(account);
 
     if (borrowers.length === 10) {
-      break
+      break;
     }
   }
-  return borrowers
+  return borrowers;
 }
 
 module.exports = {
+  findAccountById, // Add the new helper function
   findAuthorById,
   findBookById,
   partitionBooksByBorrowedStatus,
